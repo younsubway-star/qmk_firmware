c93ab2637086a2fe4d21766cf3f835b2ff11233e
lokher <lokher@gmail.com>
2025-11-19 18:37:43 +0800

    Add Q6 HE ANSI/ISO/JIS

---
+++ keyboards/keychron/q6_he/iso_encoder/iso_encoder.c
@@ -0,0 +1,209 @@
+/* Copyright 2025 @ Keychron (https://www.keychron.com)
+ *
+ * This program is free software : you can redistribute it and /or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.If not, see < http://www.gnu.org/licenses/>.
+ */
+
+#include "quantum.h"
+
+// clang-format off
+const matrix_row_t analog_matrix_mask[] = {
+    0b111111101111111111111,
+    0b111111111111111111111,
+    0b111111111111111111111,
+    0b011100001111111111111,
+    0b111101010111111111111,
+    0b001111101111001000111,
+};
+
+#ifdef RGB_MATRIX_ENABLE
+const snled27351_led_t g_snled27351_leds[RGB_MATRIX_LED_COUNT] = {
+/* Refer to SNLED27351 manual for these locations
+ *   driver
+ *   |  R location
+ *   |  |           G location
+ *   |  |           |           B location
+ *   |  |           |           | */
+    {0, CB7_CA16,   CB9_CA16,   CB8_CA16},
+    {0, CB7_CA15,   CB9_CA15,   CB8_CA15},
+    {0, CB7_CA14,   CB9_CA14,   CB8_CA14},
+    {0, CB7_CA13,   CB9_CA13,   CB8_CA13},
+    {0, CB7_CA12,   CB9_CA12,   CB8_CA12},
+    {0, CB7_CA11,   CB9_CA11,   CB8_CA11},
+    {0, CB7_CA10,   CB9_CA10,   CB8_CA10},
+    {0, CB7_CA9,    CB9_CA9,    CB8_CA9},
+    {0, CB7_CA8,    CB9_CA8,    CB8_CA8},
+    {0, CB7_CA7,    CB9_CA7,    CB8_CA7},
+    {0, CB7_CA6,    CB9_CA6,    CB8_CA6},
+    {0, CB7_CA5,    CB9_CA5,    CB8_CA5},
+    {0, CB7_CA4,    CB9_CA4,    CB8_CA4},
+    {0, CB7_CA2,    CB9_CA2,    CB8_CA2},
+    {0, CB7_CA1,    CB9_CA1,    CB8_CA1},
+    {1, CB1_CA3,    CB3_CA3,    CB2_CA3},
+    {0, CB10_CA4,   CB12_CA4,   CB11_CA4},
+    {0, CB10_CA3,   CB12_CA3,   CB11_CA3},
+    {0, CB10_CA2,   CB12_CA2,   CB11_CA2},
+    {0, CB10_CA1,   CB12_CA1,   CB11_CA1},
+
+    {0, CB1_CA16,   CB3_CA16,   CB2_CA16},
+    {0, CB1_CA15,   CB3_CA15,   CB2_CA15},
+    {0, CB1_CA14,   CB3_CA14,   CB2_CA14},
+    {0, CB1_CA13,   CB3_CA13,   CB2_CA13},
+    {0, CB1_CA12,   CB3_CA12,   CB2_CA12},
+    {0, CB1_CA11,   CB3_CA11,   CB2_CA11},
+    {0, CB1_CA10,   CB3_CA10,   CB2_CA10},
+    {0, CB1_CA9,    CB3_CA9,    CB2_CA9},
+    {0, CB1_CA8,    CB3_CA8,    CB2_CA8},
+    {0, CB1_CA7,    CB3_CA7,    CB2_CA7},
+    {0, CB1_CA6,    CB3_CA6,    CB2_CA6},
+    {0, CB1_CA5,    CB3_CA5,    CB2_CA5},
+    {0, CB1_CA4,    CB3_CA4,    CB2_CA4},
+    {0, CB1_CA3,    CB3_CA3,    CB2_CA3},
+    {0, CB1_CA2,    CB3_CA2,    CB2_CA2},
+    {0, CB1_CA1,    CB3_CA1,    CB2_CA1},
+    {1, CB1_CA2,    CB3_CA2,    CB2_CA2},
+    {0, CB10_CA8,   CB12_CA8,   CB11_CA8},
+    {0, CB10_CA7,   CB12_CA7,   CB11_CA7},
+    {0, CB10_CA6,   CB12_CA6,   CB11_CA6},
+    {0, CB10_CA5,   CB12_CA5,   CB11_CA5},
+
+    {0, CB4_CA16,   CB6_CA16,   CB5_CA16},
+    {0, CB4_CA15,   CB6_CA15,   CB5_CA15},
+    {0, CB4_CA14,   CB6_CA14,   CB5_CA14},
+    {0, CB4_CA13,   CB6_CA13,   CB5_CA13},
+    {0, CB4_CA12,   CB6_CA12,   CB5_CA12},
+    {0, CB4_CA11,   CB6_CA11,   CB5_CA11},
+    {0, CB4_CA10,   CB6_CA10,   CB5_CA10},
+    {0, CB4_CA9,    CB6_CA9,    CB5_CA9},
+    {0, CB4_CA8,    CB6_CA8,    CB5_CA8},
+    {0, CB4_CA7,    CB6_CA7,    CB5_CA7},
+    {0, CB4_CA6,    CB6_CA6,    CB5_CA6},
+    {0, CB4_CA5,    CB6_CA5,    CB5_CA5},
+    {0, CB4_CA4,    CB6_CA4,    CB5_CA4},
+    {0, CB4_CA3,    CB6_CA3,    CB5_CA3},
+    {0, CB4_CA2,    CB6_CA2,    CB5_CA2},
+    {0, CB4_CA1,    CB6_CA1,    CB5_CA1},
+    {1, CB1_CA1,    CB3_CA1,    CB2_CA1},
+    {0, CB10_CA12,  CB12_CA12,  CB11_CA12},
+    {0, CB10_CA11,  CB12_CA11,  CB11_CA11},
+    {0, CB10_CA10,  CB12_CA10,  CB11_CA10},
+    {0, CB10_CA9,   CB12_CA9,   CB11_CA9},
+
+    {1, CB1_CA16,   CB3_CA16,   CB2_CA16},
+    {1, CB1_CA15,   CB3_CA15,   CB2_CA15},
+    {1, CB1_CA14,   CB3_CA14,   CB2_CA14},
+    {1, CB1_CA13,   CB3_CA13,   CB2_CA13},
+    {1, CB1_CA12,   CB3_CA12,   CB2_CA12},
+    {1, CB1_CA11,   CB3_CA11,   CB2_CA11},
+    {1, CB1_CA10,   CB3_CA10,   CB2_CA10},
+    {1, CB1_CA9,    CB3_CA9,    CB2_CA9},
+    {1, CB1_CA8,    CB3_CA8,    CB2_CA8},
+    {1, CB1_CA7,    CB3_CA7,    CB2_CA7},
+    {1, CB1_CA6,    CB3_CA6,    CB2_CA6},
+    {1, CB1_CA5,    CB3_CA5,    CB2_CA5},
+    {1, CB1_CA4,    CB3_CA4,    CB2_CA4},
+    {1, CB10_CA9,   CB12_CA9,   CB11_CA9},
+    {1, CB10_CA8,   CB12_CA8,   CB11_CA8},
+    {1, CB10_CA7,   CB12_CA7,   CB11_CA7},
+
+    {1, CB7_CA16,   CB9_CA16,   CB8_CA16},
+    {1, CB7_CA15,   CB9_CA15,   CB8_CA15},
+    {1, CB7_CA14,   CB9_CA14,   CB8_CA14},
+    {1, CB7_CA13,   CB9_CA13,   CB8_CA13},
+    {1, CB7_CA12,   CB9_CA12,   CB8_CA12},
+    {1, CB7_CA11,   CB9_CA11,   CB8_CA11},
+    {1, CB7_CA10,   CB9_CA10,   CB8_CA10},
+    {1, CB7_CA9,    CB9_CA9,    CB8_CA9},
+    {1, CB7_CA8,    CB9_CA8,    CB8_CA8},
+    {1, CB7_CA7,    CB9_CA7,    CB8_CA7},
+    {1, CB7_CA6,    CB9_CA6,    CB8_CA6},
+    {1, CB7_CA5,    CB9_CA5,    CB8_CA5},
+    {1, CB7_CA3,    CB9_CA3,    CB8_CA3},
+    {1, CB7_CA1,    CB9_CA1,    CB8_CA1},
+    {1, CB10_CA6,   CB12_CA6,   CB11_CA6},
+    {1, CB10_CA5,   CB12_CA5,   CB11_CA5},
+    {1, CB10_CA4,   CB12_CA4,   CB11_CA4},
+    {1, CB10_CA3,   CB12_CA3,   CB11_CA3},
+
+    {1, CB4_CA16,   CB6_CA16,   CB5_CA16},
+    {1, CB4_CA15,   CB6_CA15,   CB5_CA15},
+    {1, CB4_CA14,   CB6_CA14,   CB5_CA14},
+    {1, CB4_CA10,   CB6_CA10,   CB5_CA10},
+    {1, CB4_CA7,    CB6_CA7,    CB5_CA7},
+    {1, CB4_CA6,    CB6_CA6,    CB5_CA6},
+    {1, CB4_CA5,    CB6_CA5,    CB5_CA5},
+    {1, CB4_CA4,    CB6_CA4,    CB5_CA4},
+    {1, CB4_CA2,    CB6_CA2,    CB5_CA2},
+    {1, CB4_CA1,    CB6_CA1,    CB5_CA1},
+    {1, CB7_CA2,    CB9_CA2,    CB8_CA2},
+    {1, CB10_CA2,   CB12_CA2,   CB11_CA2},
+    {1, CB10_CA1,   CB12_CA1,   CB11_CA1},
+};
+
+#define __ NO_LED
+
+led_config_t g_led_config = {
+    {
+        // Key Matrix to LED Index
+        {  0,  1,  2,  3,  4,  5,  6,  7,  8,   9,  10,  11,  12, __,  13,  14,  15,  16,  17,  18, 19 },
+        { 20, 21, 22, 23, 24, 25, 26, 27, 28,  29,  30,  31,  32, 33,  34,  35,  36,  37,  38,  39, 40 },
+        { 41, 42, 43, 44, 45, 46, 47, 48, 49,  50,  51,  52,  53, 54,  55,  56,  57,  58,  59,  60, 61 },
+        { 62, 63, 64, 65, 66, 67, 68, 69, 70,  71,  72,  73,  74, __,  __,  __,  __,  75,  76,  77, __ },
+        { 78, 79, 80, 81, 82, 83, 84, 85, 86,  87,  88,  89,  __, 90,  __,  91,  __,  92,  93,  94, 95 },
+        { 96, 97, 98, __, __, __, 99, __, __, 100, 101, 102, 103, __, 104, 105, 106, 107, 108,  __, __ },
+    },
+    {
+        // LED Index to Physical Position
+        {0, 0}, {10, 0}, {20, 0}, {31, 0}, {41, 0}, {52, 0}, {67, 0}, {78, 0}, {88, 0}, {98, 0},   {114,  0}, {124,  0}, {135,  0},             {158,  0}, {169,  0}, {179,  0},  {192,  0}, {203,  0}, {213,  0}, {224,  0},
+        {0,15}, {10,15}, {20,15}, {31,15}, {41,15}, {52,15}, {62,15}, {72,15}, {83,15}, { 93, 15}, {104, 15}, {114, 15}, {124, 15}, {140, 15},  {158, 15}, {169, 15}, {179, 15},  {192, 15}, {203, 15}, {213, 15}, {224, 15},
+        {2,28}, {15,28}, {26,28}, {36,28}, {46,28}, {57,28}, {67,28}, {78,28}, {88,28}, { 98, 28}, {109, 28}, {119, 28}, {130, 28}, {143, 33},  {158, 28}, {169, 28}, {179, 28},  {192, 28}, {203, 28}, {213, 28}, {224, 33},
+        {3,39}, {18,39}, {28,39}, {39,39}, {49,39}, {59,39}, {70,39}, {80,39}, {91,39}, {101, 39}, {111, 39}, {122, 39}, {133, 39},                                               {192, 39}, {203, 39}, {213, 39},
+        {1,52}, {12,52}, {23,52}, {33,52}, {44,52}, {54,52}, {65,52}, {75,52}, {85,52}, { 96, 52}, {106, 52}, {117, 52},            {136, 52},             {169, 52},             {192, 52}, {203, 52}, {213, 52}, {224, 58},
+        {1,64}, {14,64}, {27,64},                            {66,64},                              {105, 64}, {118, 64}, {131, 64}, {144, 64},  {158, 64}, {169, 64}, {179, 64},  {197, 64},            {213, 64},
+    },
+    {
+        // RGB LED Index to Flag
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,     1, 1, 1,  1, 1, 1, 1,
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,  1, 1, 1,  1, 1, 1, 1,
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,  1, 1, 1,  1, 1, 1, 1,
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,               1, 1, 1,
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,    1,     1,     1, 1, 1, 1,
+        1, 1, 1,          1,          1, 1, 1, 1,  1, 1, 1,  1,    1,
+    }
+};
+
+// Default Color of Per Key RGB
+#define DC_RED {HSV_RED}
+#define DC_BLU {HSV_BLUE}
+#define DC_YLW {HSV_YELLOW}
+
+HSV default_per_key_led[RGB_MATRIX_LED_COUNT] = {
+    DC_RED, DC_YLW, DC_YLW, DC_YLW, DC_YLW, DC_YLW, DC_YLW, DC_YLW, DC_YLW, DC_YLW, DC_YLW, DC_YLW, DC_YLW,          DC_YLW, DC_YLW, DC_YLW,  DC_YLW, DC_YLW, DC_YLW, DC_YLW,
+    DC_BLU, DC_BLU, DC_BLU, DC_BLU, DC_BLU, DC_BLU, DC_BLU, DC_BLU, DC_BLU, DC_BLU, DC_BLU, DC_BLU, DC_BLU, DC_YLW,  DC_YLW, DC_YLW, DC_YLW,  DC_YLW, DC_YLW, DC_YLW, DC_YLW,
+    DC_YLW, DC_BLU, DC_BLU, DC_BLU, DC_BLU, DC_BLU, DC_BLU, DC_BLU, DC_BLU, DC_BLU, DC_BLU, DC_BLU, DC_BLU, DC_RED,  DC_YLW, DC_YLW, DC_YLW,  DC_YLW, DC_YLW, DC_YLW, DC_YLW,
+    DC_YLW, DC_BLU, DC_BLU, DC_BLU, DC_BLU, DC_BLU, DC_BLU, DC_BLU, DC_BLU, DC_BLU, DC_BLU, DC_BLU, DC_BLU,                                   DC_YLW, DC_YLW, DC_YLW,
+    DC_YLW, DC_BLU, DC_BLU, DC_BLU, DC_BLU, DC_BLU, DC_BLU, DC_BLU, DC_BLU, DC_BLU, DC_BLU, DC_BLU,         DC_YLW,          DC_YLW,          DC_YLW, DC_YLW, DC_YLW, DC_RED,
+    DC_YLW, DC_YLW, DC_YLW,                         DC_BLU,                         DC_YLW, DC_YLW, DC_YLW, DC_YLW,  DC_YLW, DC_YLW, DC_YLW,  DC_YLW,         DC_YLW
+};
+
+// Default mixed RGB region
+uint8_t default_region[RGB_MATRIX_LED_COUNT] = {
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,     0, 0, 0,  0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  0, 0, 0,  0, 0, 0, 0,
+    0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  0, 0, 0,  0, 0, 0, 0,
+    0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,               0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,    0,     0,     0, 0, 0, 0,
+    0, 0, 0,          0,          0, 0, 0, 0,  0, 0, 0,  0,    0
+};
+#endif
+
---
+++ keyboards/keychron/q6_he/readme.md
@@ -0,0 +1,24 @@
+# Keychron Q6 HE
+
+![Keychron Q6 HE](https://cdn.shopify.com/s/files/1/0059/0630/1017/files/Keychron-Q6-HE-Wireless-QMK-Custom-Magnetic-Switch-Keyboard-White.jpg)
+
+A customizable 100% hall effect keyboard.
+
+* Keyboard Maintainer: [Keychron](https://github.com/keychron)
+* Hardware Supported: Keychron Q6 HE
+* Hardware Availability: [Keychron Q6 HE QMK/VIA Wireless Custom Mechanical Keyboard](https://www.keychron.com/products/keychron-q6-he-qmk-wireless-custom-keyboard)
+  Make example for this keyboard (after setting up your build environment):
+
+    make keychron/q6_he/ansi_encoder:keychron
+    make keychron/q6_he/iso_encoder:keychron
+    make keychron/q6_he/jis_encoder:keychron
+
+Flashing example for this keyboard:
+
+    make keychron/q6_he/ansi_encoder:keychron:flash
+    make keychron/q6_he/iso_encoder:keychron:flash
+    make keychron/q6_he/jis_encoder:keychron:flash
+
+**Reset Key**: Disconnect the USB cable, toggle mode switch to "Cable", hold down the *Esc* key or reset button underneath space bar, then connect the USB cable.
+
+See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
